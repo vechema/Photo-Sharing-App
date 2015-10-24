2d346852cbf9756ee88c203ab20d3e7f907fd4f2
Jo Egner <vechema@gmail.com>
2015-10-24 16:01:04 -0500

    App: Made view all streams and single stream

---
+++ AndroidDemo/app/src/main/java/com/aptmini/jreacs/connexus/Params.java
@@ -0,0 +1,8 @@
+package com.aptmini.jreacs.connexus;
+
+/**
+ * Created by Jo on 10/24/2015.
+ */
+public class Params {
+    public final static int maxStreams = 4;
+}
---
+++ AndroidDemo/app/src/main/java/com/aptmini/jreacs/connexus/ViewAStream.java
@@ -0,0 +1,128 @@
+package com.aptmini.jreacs.connexus;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.squareup.picasso.Picasso;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ViewAStream extends ActionBarActivity {
+
+    Context context = this;
+    private String TAG  = "Display A Stream";
+    String stream_name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_view_astream);
+
+        Intent intent = getIntent();
+        stream_name = intent.getStringExtra(DisplayStreams.STREAM_NAME);
+        System.out.println("ViewAStream stream name: " + stream_name);
+
+        TextView myTextView= (TextView) findViewById(R.id.stream_name_view);
+        myTextView.setText(stream_name);
+
+
+        final String request_url = "http://apt2015mini.appspot.com/mview?stream=" + stream_name;
+        AsyncHttpClient httpClient = new AsyncHttpClient();
+        httpClient.get(request_url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                final ArrayList<String> picUrls = new ArrayList<String>();
+                final ArrayList<String> picCaps = new ArrayList<String>();
+                try {
+                    JSONObject jObject = new JSONObject(new String(response));
+
+                    JSONArray displayUrls = jObject.getJSONArray("picUrls");
+                    JSONArray displayCaps = jObject.getJSONArray("picCaps");
+
+                    for(int i=0;i<displayUrls.length() && i < Params.maxStreams;i++) {
+
+                        picUrls.add(displayUrls.getString(i));
+                        picCaps.add(displayCaps.getString(i));
+
+                        System.out.println(displayUrls.getString(i));
+                    }
+                    GridView gridview = (GridView) findViewById(R.id.gridview_astream);
+                    gridview.setAdapter(new ImageAdapter(context,picUrls));
+                    gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View v,
+                                                int position, long id) {
+
+                            Toast.makeText(context, picCaps.get(position), Toast.LENGTH_SHORT).show();
+
+                            Dialog imageDialog = new Dialog(context);
+                            imageDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                            imageDialog.setContentView(R.layout.thumbnail);
+                            ImageView image = (ImageView) imageDialog.findViewById(R.id.thumbnail_IMAGEVIEW);
+
+                            Picasso.with(context).load(picUrls.get(position)).into(image);
+
+                            imageDialog.show();
+                        }
+                    });
+                }
+                catch(JSONException j){
+                    System.out.println("JSON Error");
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                Log.e(TAG, "There was a problem in retrieving the url : " + e.toString());
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.display_images, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void viewAllStreams(View view) {
+        Intent intent = new Intent(this, DisplayStreams.class);
+        startActivity(intent);
+    }
+
+}
