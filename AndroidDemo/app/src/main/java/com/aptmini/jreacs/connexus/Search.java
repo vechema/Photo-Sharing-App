ec65402bcdce052da95abd03eb306d6aab076388
Jo Egner <vechema@gmail.com>
2015-10-26 22:08:57 -0500

    Search front end

--- AndroidDemo/app/src/main/java/com/aptmini/jreacs/connexus/Search.java
+++ AndroidDemo/app/src/main/java/com/aptmini/jreacs/connexus/Search.java
@@ -25,6 +25,11 @@ public class Search extends ActionBarActivity {
     Context context = this;
     private String TAG  = "Search";
     public final static String SEARCH_TERMS = "com.aptmini.jreacs.connexus.SEARCH_TERMS";
+    public final static String STREAM_NAME = "com.aptmini.jreacs.connexus.STREAM_NAME";
+    public final static String OWNER_EMAIL = "com.aptmini.jreacs.connexus.OWNER_EMAIL";
+    public final static String RESULT_NUM = "com.aptmini.jreacs.connexus.RESULT_NUM";
+    int resultNum;
+    String search_terms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +38,25 @@ public class Search extends ActionBarActivity {
         // Get the message from the intent
         Intent intent = getIntent();
         String message = intent.getStringExtra(DisplayStreams.SEARCH_TERMS);
+        search_terms = message;
         System.out.println("WHAT WAS SEARCHED: " + message);
 
         setContentView(R.layout.activity_search);
 
-        TextView numResults = (TextView) findViewById(R.id.search_results);
+        TextView morePicsText= (TextView) findViewById(R.id.view_more_results);
+        morePicsText.setVisibility(View.VISIBLE);
+
+
+
+        String result_num = intent.getStringExtra(RESULT_NUM);
+        System.out.println("Intent result_num: " + result_num);
+        try {
+            resultNum = Integer.parseInt(result_num);
+            System.out.println("RESULTNUM WAS PARSED");
+        } catch (NumberFormatException e) {
+            System.out.println("NUMBER FORMAT EXCEPTION IN RESULT NUM");
+            resultNum = 0;
+        }
 
         final String request_url = "http://apt2015mini.appspot.com/msearch?terms="+message;
         AsyncHttpClient httpClient = new AsyncHttpClient();
@@ -53,8 +72,19 @@ public class Search extends ActionBarActivity {
                     JSONArray displayUrls = jObject.getJSONArray("coverURLs");
                     JSONArray displayNames = jObject.getJSONArray("streamNames");
                     JSONArray displayOwner = jObject.getJSONArray("ownerEmails");
+                    
+                    TextView numResults = (TextView) findViewById(R.id.search_results);
+                    numResults.setText(displayUrls.length()+" results for "+ search_terms);
 
-                    for(int i=0;i<displayNames.length() && i < Params.maxStreams;i++) {
+                    //Make the More Results button disappear if displayUrls.length < resultNum + Params.maxResults
+                    if(displayUrls.length() <= resultNum + Params.maxResults)
+                    {
+                        System.out.println("NOT ENOUGH RESULTS FOR MORE!!!");
+                        TextView myTextView= (TextView) findViewById(R.id.view_more_results);
+                        myTextView.setVisibility(View.INVISIBLE);
+                    }
+
+                    for(int i=resultNum;i<displayNames.length() && i < Params.maxResults+resultNum;i++) {
 
                         coverURLs.add(displayUrls.getString(i));
                         streamNames.add(displayNames.getString(i));
@@ -62,8 +92,9 @@ public class Search extends ActionBarActivity {
 
                         System.out.println(displayNames.getString(i));
                     }
-                    GridView gridview = (GridView) findViewById(R.id.gridview_streams);
-                    gridview.setAdapter(new ImageAdapter(context,coverURLs));
+                    GridView gridview = (GridView) findViewById(R.id.gridview_search);
+                    //gridview.setAdapter(new ImageAdapter(context,coverURLs));
+                    gridview.setAdapter(new ImageTextAdapter(Search.this, context, coverURLs, streamNames));
                     gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> parent, View v,
@@ -74,8 +105,8 @@ public class Search extends ActionBarActivity {
                             String stream_name = streamNames.get(position);
                             String owner_email = ownerEmails.get(position);
                             System.out.println("DisplayStreams, stream name: " + stream_name);
-                            //intent.putExtra(STREAM_NAME, stream_name);
-                            //intent.putExtra(OWNER_EMAIL, owner_email);
+                            intent.putExtra(STREAM_NAME, stream_name);
+                            intent.putExtra(OWNER_EMAIL, owner_email);
                             startActivity(intent);
 
                             /*Toast.makeText(context, streamNames.get(position), Toast.LENGTH_SHORT).show();
@@ -104,11 +135,27 @@ public class Search extends ActionBarActivity {
         });
     }
 
+    //This is a totally new search
     public void search(View view) {
         Intent intent = new Intent(this, Search.class);
         EditText editText = (EditText) findViewById(R.id.search_message);
         String message = editText.getText().toString();
         intent.putExtra(SEARCH_TERMS, message);
+
+        startActivity(intent);
+
+    }
+
+    public void viewMoreResults(View view) {
+        Intent intent = new Intent(this, Search.class);
+
+        intent.putExtra(SEARCH_TERMS, search_terms);
+
+        System.out.println("INTENT TO START NEARBY AGAIN, picNum: " + resultNum);
+        String toPass = ""+(resultNum+Params.maxResults);
+        System.out.println("\tWHAT I'M PASSSINGGG: " + toPass);
+        intent.putExtra(RESULT_NUM, toPass);
+
         startActivity(intent);
 
     }
